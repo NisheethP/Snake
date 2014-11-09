cd51618d8793c272a0e2eecc84d96cc9325ad0c6
Nisheeth <pandey.nisheeth59916031@gmail.com>
2014-11-09 20:03:34 +0530

    Snake now knows about bends in its tail

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -4,12 +4,12 @@
 Snake::Snake() : 
 length(3), 
 viewDir(Direction::Dir_Right),
-tail(TailVector(0))
+tail(TailVector()),
+bends(BendVector())
 {
 	for (int i = tail.size(); i < length; i++)
 		tail.push_back(SnakeTail());
 }
-
 
 Snake::~Snake()
 {
@@ -19,4 +19,36 @@ void Snake::incLength()
 {
 	if (length <= 100)
 		length++;
+}
+
+bool Snake::addBend(int len, Direction dir)
+{
+	if (len < length && dir != Dir_Error)
+	{
+		Bend tempBend(len, dir);
+		bends.push_back(tempBend);
+		return true;
+	}
+	else
+		return false;
+}
+
+bool Snake::removeBend()
+{
+	if (!bends.empty())
+	{
+		bends.pop_back();
+		return true;
+	}
+	return false;
+}
+
+Bend Snake::getBend(int num)
+{
+	if (num >= bends.size())
+	{
+		return bends.at(bends.size() - 1);
+	}
+	else
+		return bends.at(num);
 }
--- Snake/Snake.h
+++ Snake/Snake.h
@@ -6,16 +6,18 @@
 #include "SnakeTail.h"
 #include <vector>
 
-typedef std::vector<SnakeTail> TailVector;
-
 enum Direction
 {
 	Dir_Up,
-	Dir_Bottom,
+	Dir_Down,
 	Dir_Left,
 	Dir_Right,
 	Dir_Error
 };
+
+typedef std::vector<SnakeTail> TailVector;
+typedef std::pair<int, Direction> Bend;
+typedef std::vector<Bend> BendVector;
 
 class Snake
 {
@@ -24,11 +26,21 @@ private:
 	Direction viewDir;
 	SnakeHead head;
 	TailVector tail;
+	BendVector bends;
+
 public:
 	Snake();
 	~Snake();
 
 	void incLength();
+	//Adds a bend at [len] distance from the Head and the tail is pointing in [dir] behind the bend
+	bool addBend(int len, Direction dir);
+	
+	//Removes the last bend in the Snake (can't remove any intermediate bend afterall...)
+	bool removeBend();
+
+	//Gets the [num] bend in the stack by iterating over
+	Bend getBend(int num);
 };
 
 #endif
