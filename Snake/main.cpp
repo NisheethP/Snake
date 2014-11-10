9321cfbbf39860ed7e2ab128c2f33f0ffcafaf41
Nisheeth <pandey.nisheeth59916031@gmail.com>
2014-11-10 21:54:03 +0530

    Can update screen as well as move Snake simultaneously though both are in two separate loops. Sort of, maybe, successfully setup threads?

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,21 +1,76 @@
 #include "headers.h"
 #include "Constants.h"
 #include "Board.h"
-#include <thread>
+#include <boost\thread\thread.hpp>
+#include <boost\ref.hpp>
 
-void *func();
+using boost::ref;
+
+//Move the snake in block in directio it is facing with frequency of 2.5Hz
+void keepSnakeMoving(Board& board);
+
+//Update the board (with frequency of 2.5 Hz) while keepUpdating is true
+void updateScreen(Board& board, bool keepUpdating = true);
+
+//Checks for whether to keep the board updating
+void checkUpdating(bool& isUpdating);
+
+//Set if the horizontal-blinking cursor is visible or not
+void ShowConsoleCursor(bool showFlag);
+
 int main()
 {
 	SetDefaultColour();
-	Board mainBoard({1,1}, {5,5});
+	ShowConsoleCursor(false);
 
-	//std::thread snake_board(moveSnake, mainBoard);
-	mainBoard.drawBoard();
-	_getch();
-	moveSnake(mainBoard);
-	mainBoard.drawBoard();
-	
-	
+	Board mainBoard({1,1}, {5,5});
+	bool isUpdating = true;
+
+	boost::thread MoveSnake(ref(keepSnakeMoving), ref(mainBoard));
+	boost::thread BoardUpdate(ref(updateScreen), ref(mainBoard), ref(isUpdating));
+	boost::thread CheckBoardUpdate(checkUpdating, ref(isUpdating));
+
+	MoveSnake.join();
+	BoardUpdate.join();
+	CheckBoardUpdate.join();
+
 	PressAnyKey();
 	return 0;
+}
+
+//Move the snake in block in directio it is facing with frequency of 2.5Hz
+void keepSnakeMoving(Board& board)
+{
+	while (board.getIsSnakeMoving())
+	{
+		Sleep(400);
+		board.moveSnake();
+	}
+}
+
+//Update the board (with frequency of 5 Hz) while keepUpdating is true
+void updateScreen(Board& board, bool keepUpdating)
+{
+	while (keepUpdating)
+	{
+		Sleep(200);
+		board.drawBoard();
+	}
+}
+
+//Set if the horizontal-blinking cursor is visible or not
+void ShowConsoleCursor(bool showFlag)
+{
+	CONSOLE_CURSOR_INFO     cursorInfo;
+
+	GetConsoleCursorInfo(Global::hStdout, &cursorInfo);
+	cursorInfo.bVisible = showFlag; // set the cursor visibility
+	SetConsoleCursorInfo(Global::hStdout, &cursorInfo);
+}
+
+//Checks for whether to keep the board updating
+void checkUpdating(bool& isUpdating)
+{
+	_getch();
+	isUpdating = false;
 }
